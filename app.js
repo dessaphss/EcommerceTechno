74f8e6d1346ec2f4bbc21c24db0f393b6ac41e21
Andressa Paranhos <andressaparanhos1@hotmail.com>
2023-02-17 20:29:10 -0300

    method fetch criado para puxar os produtos

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const vm = new Vue ({
   el: "#app",
   data: {
+    produtos: []
+  },
+  methods: {
+    fetchProdutos(){
+      fetch("./api/produtos.json")
+      .then(r => r.json())
+      .then(r => {
+        this.produtos = r;
+      })
+    }
   }
 });
